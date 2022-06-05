f30db9710a673a6884e655697719250c2245a165
Ivan <example@gmail.com>
2022-06-05 12:07:39 +0300

    first_task_commit

--- js/scriptt.js
+++ js/scriptt.js
@@ -17,22 +17,26 @@ console.log(isChecked || !isClose); */
 
 
 
-function addition(personAge, personName, object, firstNum, secondNum) {
-    if (personAge > 20) {
-        console.log(`Welcome ${personName}, it's your time!`);
-        if (object == '+') {
-            console.log("The answer is : ", firstNum + secondNum);
-        }
-        else if (object == '-') {
-            console.log("The answer is : ", firstNum - secondNum);
-        }
-        else {
-            console.log("BUT...CALCULATOR IS NOT AVAILABLE")
-        }
-    }
-    else {
-        console.log("Your age is not good for this page!")
-    }
-}
+// Hometask first 
 
-addition(26, 'Ivan', '-', 14, 16);
+//first task
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", '')
+
+
+//second task
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+        
+    },
+    actors: {
+
+    },
+    genres: [],
+    privat: false
+};
+
+const q1 = prompt("Один из последних просмотренных фильмов?",'');
+const q2 = prompt("На сколько оцените его ?",'');
+personalMovieDB.movies[q1] = q2;
+console.log(personalMovieDB);
