0f43fae2921c18db04de628a7a405cb2e39819b0
Ivan <example@gmail.com>
2022-06-06 13:13:54 +0300

    nav_menu_test

--- js/test.js
+++ js/test.js
@@ -26,13 +26,63 @@ console.log(5 + '6');
 
 //Условие 
 
-/* let num = 101;
-(num == 100) ? console.log("ok"): console.log("Erorr"); */
+const num = 111;
 
-/* if (num = 100) {
+/* (num === 100) ? console.log("ok"): console.log("Erorr");
+
+if (num === 100) {
     console.log("ok");
 }
 else {
     console.log("Error");
+} */
+
+switch (num) {
+    case 99:
+        console.log("its true number")
+        break;
+    case 100:
+        console.log("Great its your number");
+        break;
+    default:
+        console.log("try to do this later");
+        break;
 }
- */
+
+
+
+let firstNum = 10;
+let secondNum = 22;
+
+(firstNum != 20 && secondNum != 22) ? console.log("right") : console.log("isnt right");
+
+
+
+
+
+console.log( NaN || 2 || undefined ); // 2
+ 
+console.log( NaN && 2 && undefined ); //NaN
+ 
+console.log( 1 && 2 && 3 ); // 3
+ 
+console.log( !1 && 2 || !3 ); // false
+ 
+console.log( 25 || null && !3 );// 25
+ 
+console.log( NaN || null || !3 || undefined || 5); //5
+ 
+console.log( NaN || null && !3 && undefined || 5); // 5
+ 
+console.log( 5 === 5 && 3 > 1 || 5);// 5
+
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+}
+
